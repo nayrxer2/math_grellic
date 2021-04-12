de5eb4bc8107ae6d0ea75e195bc851b1fd6710a1
Mae Dagum <mayeghen@gmail.com>
2021-04-13 00:16:17 +0800

    Fixed Splash Button Component

--- src/components/landingpage/LandingPageSplash.js
+++ src/components/landingpage/LandingPageSplash.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import '../../styles/landingpagesplash.css'
 import logo from '../../images/logo-brand.png'
+import { Button } from '../Button'
 
 const LandingPageSplash = () => {
     return (
-        <div className="container justify-content-around col-9">
+        <div className="container-fluid justify-content-around col-9">
             <div className="col-6">
                 <img className="lp-logo-brand" src={logo} />
                 <h1 className="main-info headline-h1"> MATH GRELLIC</h1>
                 <p className="main-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.</p>
-                <button type="button" className="btn btn-primary btn-enrol-here"><ion-icon name="arrow-back" /> ENROLL HERE</button>
+                <Button type="button">Enroll Here</Button>
             </div>
         </div> 
     )
