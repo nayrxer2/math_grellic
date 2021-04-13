8101f6f84075c039a9dbd93991157ebe2d74b81b
Mae Dagum <mayeghen@gmail.com>
2021-04-13 15:48:07 +0800

    Implemented Text Styles in Section Dashboard

--- src/components/landingpage/LandingPageDashboard.js
+++ src/components/landingpage/LandingPageDashboard.js
@@ -4,6 +4,7 @@ import imglinebottom from '../../images/lines-bottom.png'
 import imgdashboard  from '../../images/dashboard.png'
 import bgcolor from '../../images/bg-dashboard.png'
 import '../../styles/landingpagesdashboard.css'
+import { LinkButton } from '../LinkButton'
 
 
 export default function LandingPageDashboard() {
@@ -12,16 +13,18 @@ export default function LandingPageDashboard() {
             <div className="row col-md-4">
                 <img className="lines-top content-to-hide" src={imglinetop} />
             </div>
+
             <div className="row col-md-12 ml-md-auto dashboard-img">
                 <div className="col-8 col-md-7"><img className="img-fluid" src={bgcolor} /></div>
                 <div className="col-4 col-md-5 align-self-center col-content">
-                    <p className="">Student / Professor Dashboard</p>
-                    <h1 className="">Dashboard. <br/>Organize, Prioritize and Learn with Ease</h1>
-                    <p className="">Let’s Get Started</p>
-                </div>
+                    <p className="subheading">Student / Professor Dashboard</p>
+                    <div className="h3-heading text-high"><span className="text-accent">Dashboard. </span><br/>Organize, Prioritize<br/>and Learn with Ease</div> 
+                    <LinkButton buttonBg="dark" buttonSize="large=lnkbtn">Let's Get Started</LinkButton>
+                </div> 
             </div>
+
             <div className="row col-md-6 offset-md-1">
-                <img className="" src={imglinebottom} />
+                <img className="lines-bottom " src={imglinebottom} />
             </div> 
         </div>
     )
