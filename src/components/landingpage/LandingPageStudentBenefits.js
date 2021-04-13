cc7c693268d3f1eed3933850c86caec912360354
ryan <ryangimotea9@gmail.com>
2021-04-14 01:52:57 +0800

    Excluded & changed CTA section in landing page + Added initial footer section
    Applied mae's components style for texts

--- src/components/landingpage/LandingPageStudentBenefits.js
+++ src/components/landingpage/LandingPageStudentBenefits.js
@@ -7,21 +7,21 @@ import '../../styles/landingpagestudentsbenefits.css'
 export default function LandingPageStudentBenefits() {
     return (
         <div className="container-fluid col-9">
-            <h1>Benefits of learning with us</h1>
+            <p className="h2-heading">Benefits of <span className="text-accent">learning</span> with us</p>
             <div className="row">
                 <div className="col-md-auto">
                     <img src={icontimezone}></img>
                 </div>
                 <div className="col">
-                    <h4>Time zones ain’t no thing</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+                    <p className="h5-heading">Time zones ain’t no thing</p>
+                    <p className="lead-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
                 </div>
                 <div className="col-md-auto">
                     <img src={iconspectrum}></img>
                 </div>
                 <div className="col">
-                    <h4>Full Spectrum of Lessons</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+                    <p className="h5-heading">Full Spectrum of Lessons</p>
+                    <p className="lead-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
                 </div>
             </div>
             <div className="row">
@@ -29,15 +29,15 @@ export default function LandingPageStudentBenefits() {
                     <img src={iconmap}></img>
                 </div>
                 <div className="col">
-                    <h4>Impossible? We’re on it</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+                    <p className="h5-heading">Impossible? We’re on it</p>
+                    <p className="lead-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
                 </div>
                 <div className="col-md-auto">
                     <img src={iconstudy}></img>
                 </div>
                 <div className="col">
-                    <h4>Flexible Study Terms</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+                    <p className="h5-heading">Flexible Study Terms</p>
+                    <p className="lead-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
                 </div> 
             </div>
         </div>
