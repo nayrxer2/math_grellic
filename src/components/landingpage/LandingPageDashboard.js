9b7bc919444e1cf1159ca767d14cbaa554bfbdfb
Mae Dagum <mayeghen@gmail.com>
2021-04-13 16:49:51 +0800

    Minor Fixes in Section Dashboard

--- src/components/landingpage/LandingPageDashboard.js
+++ src/components/landingpage/LandingPageDashboard.js
@@ -9,22 +9,22 @@ import { LinkButton } from '../LinkButton'
 
 export default function LandingPageDashboard() {
     return (
-        <div className="row col-12"> 
+        <div className="row col-12 pl-0"> 
             <div className="row col-md-4">
-                <img className="lines-top content-to-hide" src={imglinetop} />
+                <img className="lines" src={imglinetop} />
             </div>
 
             <div className="row col-md-12 ml-md-auto dashboard-img">
-                <div className="col-8 col-md-7"><img className="img-fluid" src={bgcolor} /></div>
+                <div className="col-8 col-md-7 pl-0"><img className="img-fluid" src={bgcolor} /></div>
                 <div className="col-4 col-md-5 align-self-center col-content">
                     <p className="subheading">Student / Professor Dashboard</p>
-                    <div className="h3-heading text-high"><span className="text-accent">Dashboard. </span><br/>Organize, Prioritize<br/>and Learn with Ease</div> 
+                    <div className="h3-heading text-high mb-3"><span className="text-accent">Dashboard. </span><br/>Organize, Prioritize<br/>and Learn with Ease</div> 
                     <LinkButton buttonBg="dark" buttonSize="large=lnkbtn">Let's Get Started</LinkButton>
                 </div> 
             </div>
 
             <div className="row col-md-6 offset-md-1">
-                <img className="lines-bottom " src={imglinebottom} />
+                <img className="lines-bottom" src={imglinebottom} />
             </div> 
         </div>
     )
