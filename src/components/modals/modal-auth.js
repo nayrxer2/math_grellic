71f5333ddf7f67ae203c320eadc25d51f1c24479
link <archiehabaradas3228@gmail.com>
2021-04-15 14:52:57 +0800

    Sign up UI almost done waiting for the background image

--- src/components/modals/modal-auth.js
+++ src/components/modals/modal-auth.js
@@ -13,6 +13,7 @@ const Modal = forwardRef((props, ref) => {
     const [showLogin, setShowLogin] = React.useState(false)
     const [showTeacherLogin, setshowTeacherLogin] = React.useState(false)
     const [showStudentLogin, setShowStudentLogin] = React.useState(false)
+    const [showSignup, setShowSignup] = React.useState(false)
 
     useImperativeHandle(ref, () => {
         return {
@@ -29,6 +30,7 @@ const Modal = forwardRef((props, ref) => {
         setDisplay(false)
         setShowSelection(true)
         setShowLogin(false)
+        setShowSignup(false)
     }
 
     const selection = (currentUser) => {
@@ -38,6 +40,7 @@ const Modal = forwardRef((props, ref) => {
       }else{
         setShowSelection(true)
         setShowLogin(false)
+        setShowSignup(false)
       }
 
       if (currentUser == "Teacher"){
@@ -47,6 +50,17 @@ const Modal = forwardRef((props, ref) => {
         setshowTeacherLogin(false)
         setShowStudentLogin(true)
       }
+    }
+
+    const signup = () => {
+      if (showSignup){
+        setShowSignup(false)
+        setShowLogin(true)
+      }else{
+        setShowSignup(true)
+        setShowLogin(false)
+      }
+      
     }
 
     if (display) {
@@ -107,19 +121,77 @@ const Modal = forwardRef((props, ref) => {
                               <label for="formGroupExampleInput2" class="form-label label-med">Password</label>
                               <input type="text" class="form-control col-md-4" id="formGroupExampleInput2"/>
                             </div>
-                            <div className="login-submit">
+                            <div className="auth-submit">
                               <Button type="submit" buttonStyle="primary">LOGIN</Button>  
                             </div>                       
                           </form>
                           {
-                            showTeacherLogin ? <p className="body-2">Don't have an account yet? <i className="label-med login-user-label">Sign Up</i></p> : null
+                            showTeacherLogin ? <p className="body-2">Don't have an account yet? <i onClick={() => signup()} className="label-med login-user-link">Sign Up</i></p> : null
                           }
                         </div>
                       </div>
                     </div>
                   </div>                 
                   : null
-                }               
+                } 
+                {/* Signup*/}
+                {
+                  showSignup ?
+                  <div className={"modal-login-box bg-login"}>
+                    <div >
+                      <div className="modal-selection-close"><Button onClick={() => close()} buttonStyle="tertiary" ><i class="bi bi-x"></i>Close</Button></div>
+                      <div><Button onClick={() => selection()} buttonStyle="tertiary" ><i class="bi bi-arrow-left-short"></i>BACK TO SELECTION</Button></div>                    
+                      
+                      <div id="login-container" className="modal-auth-footer">
+                        <div className="login-user">
+                          <h1 className="h3-heading">Sign Up</h1>
+                          <p className="body-2">Let's sign you up! Please fill in the fields to start.</p>
+
+                          <form class="row g-3">
+                          <div class="col-md-4">
+                            <label for="validationDefault01" class="form-label label-med">First name</label>
+                            <input type="text" class="form-control" id="validationDefault01" required/>
+                          </div>
+                          <div class="col-md-4">
+                            <label for="validationDefault02" class="form-label label-med">Last name</label>
+                            <input type="text" class="form-control" id="validationDefault02" required/>
+                          </div>
+                          <div class="col-md-4">
+                          <label for="validationDefault03" class="form-label label-med">Teacher's ID</label>
+                          <div class="input-group">
+                            <input type="text" class="form-control" id="validationDefault03"  aria-describedby="inputGroupPrepend2" required/>
+                          </div>
+                          </div>
+                          <div class="col-md-6 sign-up-form-spacing">
+                            <label for="validationDefaultUsername" class="form-label label-med">Username</label>
+                            <input type="text" class="form-control" id="validationDefaultUsername" required/>
+                          </div>
+                          <div class="col-md-6 sign-up-form-spacing">
+                            <label for="validationDefault05" class="form-label label-med">Email Address</label>
+                            <input type="text" class="form-control" id="validationDefault05" required/>
+                          </div>
+                          <div class="w-100"></div>
+                          <div class="col-md-6 sign-up-form-spacing">
+                            <label for="validationDefault03" class="form-label label-med">Password</label>
+                            <input type="text" class="form-control" id="validationDefault03" required/>
+                          </div>
+                          <div class="col-md-6 sign-up-form-spacing">
+                            <label for="validationDefault05" class="form-label label-med">Confirm Password</label>
+                            <input type="text" class="form-control" id="validationDefault05" required/>
+                          </div>  
+                          <div className="auth-submit col-12">
+                            <Button type="submit" buttonStyle="primary">SIGN UP</Button>  
+                          </div>                       
+                          </form>
+                          {
+                            showTeacherLogin ? <p className="body-2">Already have an account? <i onClick={() => signup()} className="label-med login-user-link">Log In</i></p> : null
+                          }
+                        </div>
+                      </div>
+                    </div>
+                  </div>                 
+                  : null
+                }
             </div>, document.getElementById("modal-root"))
     }
 
