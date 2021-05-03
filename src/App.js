79e5f4cce42803e5707b8f5f62d036a5f68fdf46
ryan <ryangimotea9@gmail.com>
2021-05-04 00:19:14 +0800

    Fixed content and added some features
    added Initial Student Dashboard & react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,20 +5,31 @@ import LandingPageBenefits from './components/landingpage/LandingPageBenefits'
 import LandingPageCTA from './components/landingpage/LandingPageCTA'
 import LandingSectionFeatures from './components/landingpage/LandingSectionFeatures'
 import LandingPageFooter from './components/landingpage/LandingPageFooter'
+// dashboard
+import StudentDashboard from './components/dashboard/StudentDashboard'
 import "bootstrap/dist/css/bootstrap.min.css"
 import './styles/main.css'
+import 'bootstrap'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-  <div className="bg-splash">  
-    <Navbar />  
-      <LandingPageSplash />
-      <LandingPageSectionAbout />
-      <LandingPageBenefits />
-      <LandingSectionFeatures />
-      <LandingPageCTA />
-      <LandingPageFooter />  
-  </div>
+    <Router>  
+      <Switch>
+        <Route exact path="/home">
+        <div className="bg-splash">
+          <Navbar />  
+          <LandingPageSplash />
+          <LandingPageSectionAbout />
+          <LandingPageBenefits />
+          <LandingSectionFeatures />
+          <LandingPageCTA />
+          <LandingPageFooter />
+        </div>
+        </Route>
+        <Route exact path="/student" component={StudentDashboard} />
+      </Switch>
+    </Router>
   );
 }
 
